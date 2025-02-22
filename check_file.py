7aece21dd80638cc496fb01053ee3a3e03aa0aaa
the-ephemeral-life <arorahimank17@gmail.com>
2025-02-23 01:34:19 +0530

    Add file to check malware using hashfile

---
+++ check_file.py
@@ -0,0 +1,33 @@
+import hashlib
+from app import re
+def calculate_file_hash(file_path):
+    try:
+        hasher = hashlib.sha256()
+        with open(file_path, "rb") as f:
+            for chunk in iter(lambda: f.read(4096), b""):
+                hasher.update(chunk)
+        return hasher.hexdigest()
+    except Exception as e:
+        print(f"Error calculating hash for {file_path}: {e}")
+        return None
+
+def check_is_malicious(file_path, known_hashes):
+    try:
+        file_hash = calculate_file_hash(file_path)
+        
+        if file_hash in known_hashes:
+            print(f"[ALERT] {file_path} is malicious (Hash match found)!")
+            return True
+        else:
+            print(f"[SAFE] {file_path} appears to be clean.")
+    except Exception as e:
+        print(f"Error scanning {file_path}: {e}")
+
+def check_file(file_to_scan):
+    counter = 1
+    while counter <= 6:
+        if check_is_malicious(file_to_scan, open(f"Malware-Hash-Database\\SHA256\\sha256_hashes_{counter}.txt","r")):
+            return True
+        counter += 1
+    return False
+    
